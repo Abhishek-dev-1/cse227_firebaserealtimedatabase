2a4fbf3fa8bdddea6f816bbb1ed6c311c7c570be
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Air.local>
2025-01-30 13:36:25 +0530

    Initial commit

---
+++ app/src/main/java/EmployeeModel.kt
@@ -0,0 +1,8 @@
+package com.example.cse227_firebaserealtimedatabase
+
+data class EmployeeModel(
+    var empID:String?=null,
+    var empName:String?=null,
+    var empAge:String?=null,
+    var empSalary:String?=null
+)
---
+++ app/src/main/java/com/example/cse227_firebaserealtimedatabase/FetchingActivity.kt
@@ -0,0 +1,16 @@
+package com.example.cse227_firebaserealtimedatabase
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FetchingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fetching)
+
+    }
+}
